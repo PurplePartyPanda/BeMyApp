5b0ca14d29440d0a04b65c48fcaa754015c5512c
wil <wilz.wong@mail.utoronto.ca>
2013-02-09 13:17:45 +0100

    merged all conflicts
    booo yeah mudafuckaaas!

--- buttons.lua
+++ buttons.lua
@@ -41,3 +41,5 @@ function Button:touch(event)
     --    end
     --end
 end
+
+
--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
 -- PURPLE PARTY PANDA
+<<<<<<< HEAD
 
 require("buttons")
 require("guisetup")
@@ -7,6 +8,19 @@ function init()
   buildDanceGui(0)
   -- print(display.contentWidth)
   -- print(display.contentHeight)
+=======
+-- NOTE!!!!
+-- in LUA, 
+-- the variable's default scope seems to be global
+-- to delete a golbal variable, assign nil to it
+-- in LUA, arrays starts from index 1 instead of 0
+require("danceBattle")
+
+function init()
+  -- add listener of the 6 buttons
+  -- add listener of tilting phone
+  buildDanceGui(0)
+>>>>>>> modulaizing danceBattle GUI
 end
 
 init()
