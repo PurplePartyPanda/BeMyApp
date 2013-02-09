2c1e77bfae8384714159317c452b6bdd7f29117f
wil <wilz.wong@mail.utoronto.ca>
2013-02-09 17:43:07 +0100

    menu function wrappers

--- main.lua
+++ main.lua
@@ -2,15 +2,31 @@
 require("danceBattle")
 require("buttons")
 
-function onButtonPlayTap(time, self)
+function onButtonMenuTap(time, self)
+  -- check self
+  if self.id == "play" then
+    --destroy the main menu
     Button.destroy(self)
+    --and then show game
     buildDanceGui(0)
+  elseif self.id == "options" then
+    -- show options overlay
+  elseif self.id == "levels" then
+    -- show level overlay
+  elseif self.id == "credits" then
+    -- show credit overlay
+  elseif self.id == "exit" then
+    -- close app
+  else
+    print("it's another button")
+  end
+
 end
 
 function init()
   -- add listener of the 6 buttons
   -- add listener of tilting phone
-  buttonPlay = Button.create("play", 0, 0, "images/button_a.png", "images/button_a_over.png", onButtonPlayTap)
+  buttonPlay = Button.create("play", 0, 0, "images/button_a.png", "images/button_a_over.png", onButtonMenuTap)
   buttonPlay.button.x = display.contentWidth / 2
   buttonPlay.button.y = display.contentHeight / 2
 end
