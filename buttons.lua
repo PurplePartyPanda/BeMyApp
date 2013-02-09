666863a3727916a47a2ff5299bf2697aad0b6966
Sacaldur <sacaldur@gmx.net>
2013-02-09 11:21:31 +0100

    created class for buttons
    Now the player don't have to keep his finger exactly on the same
    position. It works, I don't now why, but it's ok...

--- buttons.lua
+++ buttons.lua
@@ -1,3 +1,5 @@
+Button = {button = nil, callBack=nil}
+Button.__index = Button
 
 -- creates a button
 --   x: horizontal index, 0=left, 1=right
@@ -5,18 +7,37 @@
 --   image: the button image (without touch effect)
 --   hoverimage: the button image with press effect
 --   callback: the function to call when the button is pressed
-function createButton(x, y, image, hoverimage, callback)
+function Button.create(x, y, image, hoverimage, callback)
+    local btn = {}            -- our new object
+    setmetatable(btn,Button)  -- make Button handle lookup
+    --self.__index = self
+    
     local widget = require "widget"
     local buttonwidth = (display.contentWidth - display.contentHeight) / 2
     local buttonheight = display.contentHeight / 3
-    local button = widget.newButton{
+    btn.button = widget.newButton{
         default = image,
         over = hoverimage,
         width = buttonwidth,
         height = buttonheight
     }
-    button.x = (display.contentWidth - buttonwidth) * x + buttonwidth / 2
-    button.y = buttonheight * y + buttonheight / 2
-    button:addEventListener("tap", callback)
-	return button
+    btn.button.x = (display.contentWidth - buttonwidth) * x + buttonwidth / 2
+    btn.button.y = buttonheight * y + buttonheight / 2
+    btn.callBack = callback
+    btn:init()
+    return btn
+end
+
+function Button:init()
+    self.button:addEventListener("touch", self)
+end
+
+function Button:touch(event)
+    if event.xStart - event.x >= -32 and event.xStart - event.x <= 32 then
+        if event.yStart - event.y >= -32 and event.yStart - event.y <= 32 then
+            if event.phase == "release" then
+			    self.callBack(event.time)
+            end
+        end
+    end
 end
--- main.lua
+++ main.lua
@@ -5,37 +5,37 @@ require("buttons")
 local textObject = display.newText(display.contentWidth .. "x" .. display.contentHeight, 160, 50, nil, 24 )
 textObject:setTextColor( 255,255,255 )
 
-function onButtonATap(event)
-    print("button a")
+function onButtonATap(time)
+    textObject.text = "button a"
 end
 
-function onButtonBTap(event)
-    print("button b")
+function onButtonBTap(time)
+    textObject.text = "button b"
 end
 
-function onButtonCTap(event)
-    print("button c")
+function onButtonCTap(time)
+    textObject.text = "button c"
 end
 
-function onButtonDTap(event)
-    print("button d")
+function onButtonDTap(time)
+    textObject.text = "button d"
 end
 
-function onButtonETap(event)
-    print("button e")
+function onButtonETap(time)
+    textObject.text = "button e"
 end
 
-function onButtonFTap(event)
-    print("button f")
+function onButtonFTap(time)
+    textObject.text = "button f"
 end
 
 function init()
-  local buttona = createButton(0, 0, "images/button_a.png", "images/button_a_over.png", onButtonATap)
-  local buttonb = createButton(0, 1, "images/button_b.png", "images/button_b_over.png", onButtonBTap)
-  local buttonc = createButton(0, 2, "images/button_c.png", "images/button_c_over.png", onButtonCTap)
-  local buttond = createButton(1, 0, "images/button_d.png", "images/button_d_over.png", onButtonDTap)
-  local buttone = createButton(1, 1, "images/button_e.png", "images/button_e_over.png", onButtonETap)
-  local buttonf = createButton(1, 2, "images/button_f.png", "images/button_f_over.png", onButtonFTap)
+  local buttona = Button.create(0, 0, "images/button_a.png", "images/button_a_over.png", onButtonATap)
+  local buttonb = Button.create(0, 1, "images/button_b.png", "images/button_b_over.png", onButtonBTap)
+  local buttonc = Button.create(0, 2, "images/button_c.png", "images/button_c_over.png", onButtonCTap)
+  local buttond = Button.create(1, 0, "images/button_d.png", "images/button_d_over.png", onButtonDTap)
+  local buttone = Button.create(1, 1, "images/button_e.png", "images/button_e_over.png", onButtonETap)
+  local buttonf = Button.create(1, 2, "images/button_f.png", "images/button_f_over.png", onButtonFTap)
   -- print(display.contentWidth)
   -- print(display.contentHeight)
 end
