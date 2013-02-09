6bc017593f96440c5c4cb4168d5ceb1a9af80775
Sacaldur <sacaldur@gmx.net>
2013-02-09 16:48:12 +0100

    placing start button in the middle of the screen

--- main.lua
+++ main.lua
@@ -11,8 +11,8 @@ function init()
   -- add listener of the 6 buttons
   -- add listener of tilting phone
   buttonPlay = Button.create("play", 0, 0, "images/button_a.png", "images/button_a_over.png", onButtonPlayTap)
-  buttonPlay.button.x = 450
-  buttonPlay.button.y = 240
+  buttonPlay.button.x = display.contentWidth / 2
+  buttonPlay.button.y = display.contentHeight / 2
 end
 
 init()
